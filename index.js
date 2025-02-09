8d2be2f1278be01fb0ae8290ebcd7f167727b277
Rob <robert.chamberlain@cmeducations.se>
2025-02-09 19:34:25 +0100

    Added dotenv

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express"
 import godRouter from "./routes/godRouter.js";
 import heroRouter from "./routes/heroRouter.js";
+import dotenv from "dotenv"
 
 const app = express();
+
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs")
